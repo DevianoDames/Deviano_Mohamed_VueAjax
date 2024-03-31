f5f2aa3b1a54ad87692803499efe52f1ccd0e87b
DevianoDames <d_dames183654@fanshaweonline.ca>
2024-03-31 15:47:31 -0400

    added lumen

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const bookStore = Vue.createApp({
+const mangaStore = Vue.createApp({
     created() {
         fetch('http://localhost:8888/bookstore-api/public/books')
         .then(res => res.json())
         .then(data =>{
             console.log(data)
-            this.booksData = data;
+            this.mangasData = data;
         })
         .catch(error => {
             console.log(error);
@@ -13,7 +13,7 @@ const bookStore = Vue.createApp({
     },
     data() {
         return {
-            booksData: [],
+            mangasData: [],
             firstSentence:"",
             ratingsAverage:"",
             authorName:"",
@@ -22,9 +22,9 @@ const bookStore = Vue.createApp({
         }
     },
     methods: {
-        getBook(whichBook){
-            console.log(whichBook);
-            let title = whichBook;
+        getManga(whichManga){
+            console.log(whichManga);
+            let title = whichManga;
             let convertedTilte = title.split(' ').join('+');
             console.log(convertedTilte);
             fetch(`https://openlibrary.org/search.json?q=${convertedTilte}`)
@@ -36,12 +36,12 @@ const bookStore = Vue.createApp({
                 if(data.docs.length > 0) {
                     this.error = false;
                                         //condition ? trueExpression : falseExpression
-                    this.firstSentence = book.first_sentence ? book.first_sentence[0]: 'Not available';
-                    this.ratingsAverage = book.ratings_average ? book.ratings_average.toFixed(2): 'Not available';
-                    this.authorName = book.author_name? book.author_name[0] : 'Not available';
-                    this.numberOfPages = book.number_of_pages_median ? book.number_of_pages_median : 'Not available';
+                    this.firstSentence = manga.first_sentence ? manga.first_sentence[0]: 'Not available';
+                    this.ratingsAverage = manga.ratings_average ? manga.ratings_average.toFixed(2): 'Not available';
+                    this.mangakaName = manga.mangaka_name? manga.mangaka_name[0] : 'Not available';
+                    this.numberOfPages = manga.number_of_pages_median ? manga.number_of_pages_median : 'Not available';
                 }else {
-                    this.error = "No Book Was Found. Please Try Again";
+                    this.error = "No Manga Was Found. Please Try Again";
                 }
             })
             .catch(error => {
@@ -51,4 +51,4 @@ const bookStore = Vue.createApp({
         }
     }
 });
-bookStore.mount("#app");
+mangaStore.mount("#app");
